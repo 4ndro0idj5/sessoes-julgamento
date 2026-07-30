42c3c3b1c6017d86d412e1d694759532bfcca369
Spider <4ndro0idj5@gmail.com>
2026-07-30 15:01:40 -0300

    corrigir-inicializacao-do-perfil-dev

--- src/main/java/br/jus/sessoes/repository/SessaoRepository.java
+++ src/main/java/br/jus/sessoes/repository/SessaoRepository.java
@@ -3,10 +3,5 @@ package br.jus.sessoes.repository;
 import br.jus.sessoes.domain.Sessao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public interface SessaoRepository extends JpaRepository<Sessao, Long> {
-
-    List<Sessao> findByDataBetweenOrderByDataAscHorarioAsc(LocalDate inicio, LocalDate fim);
 }
